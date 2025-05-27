d18a598f127cd75a447f48123f1d057fa4ad4dcf
GTbetting <contato@gtbetting.com.br>
2025-05-27 17:36:50 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react'
+
 export default function App() {
   return (
     <div>
-      <h1>GTBetting</h1>
-      <p>App carregado com sucesso!</p>
+      <h1>GTBetting está no ar!</h1>
     </div>
   )
 }
