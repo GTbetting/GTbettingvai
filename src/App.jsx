c92c52351ef73620dd2f91984d53daef6168d809
GTbetting <contato@gtbetting.com.br>
2025-05-28 09:28:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        {/* futuras rotas: /home, /admin, etc */}
       </Routes>
     </Router>
   );
